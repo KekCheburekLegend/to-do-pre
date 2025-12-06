a55224d9cb7606cc355ea3928a32f7a941b5323e
Iskander Khanov <needdocer@gmail.com>
2025-12-07 00:42:53 +0500

    complete task index.js and make task to-do list

--- index.js
+++ index.js
@@ -12,30 +12,65 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-	return items;
+	if (localStorage.getItem('tasks') === null) {	
+    return items;
+	}
+	else {
+    return JSON.parse(localStorage.getItem('tasks'));
+	}
 }
 
 function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text").textContent = item;
+  const textElement = clone.querySelector(".to-do__item-text");
+  textElement.textContent = item
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+  
+  deleteButton.addEventListener('click', () => {
+	clone.remove();
+	items = getTasksFromDOM()
+	saveTasks(items)
+  })
+
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+
+  duplicateButton.addEventListener('click', () => {
+	const itemName = textElement.textContent;
+	const newItem = createItem(itemName); // createItem(item) <= передает item (значение в поле)
+	listElement.prepend(newItem);
+	items = getTasksFromDOM();
+	saveTasks(items);
+  })
+
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
+  
+  editButton.addEventListener('click', (evt) => {
+	evt.preventDefault;
+	textElement.setAttribute('contenteditable', 'true') // право на редактирование текста элемента *_*
+	textElement.focus()
+	})
+	textElement.addEventListener('blur', () => {
+		textElement.setAttribute('contenteditable','false')
+		items = getTasksFromDOM()
+		saveTasks(items)
+		}); //blur потеря фокуса :)
+
+
   return clone;
 }
 
 function getTasksFromDOM() {
 	const tasks = [];
 	const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
-	itemsNamesElements.forEach((items) => {
-		tasks.push(items.textContent)
+	itemsNamesElements.forEach((item) => {
+		tasks.push(item.textContent)
 	})
 	return tasks
 }
 
 function saveTasks(tasks) {
-	
+	localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 items = loadTasks()
@@ -49,4 +84,6 @@ formElement.addEventListener('submit', function(evt){
 	const item = inputElement.value;
 	listElement.prepend(createItem(item));
 	inputElement.value = '';
+	items = getTasksFromDOM()
+	saveTasks(items)
 })
